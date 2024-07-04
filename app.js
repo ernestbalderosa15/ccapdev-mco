620a0ce3eaad8a4231d0136bde214ed050eeb412
santosmarcus <gmarcus.santos@gmail.com>
2024-07-04 22:56:55 +0800

    hbs test

---
+++ app.js
@@ -0,0 +1,30 @@
+//Install Command:
+//npm init -y
+//npm i express express-handlebars body-parser mongoose
+
+const express = require('express');
+const server = express();
+
+const bodyParser = require('body-parser')
+server.use(express.json()); 
+server.use(express.urlencoded({ extended: true }));
+
+const handlebars = require('express-handlebars');
+server.set('view engine', 'hbs');
+server.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+server.use(express.static('public'));
+
+server.get('/', function(req, resp){
+    resp.render('main',{
+        layout: 'index',
+        title: 'The Forum',
+    });
+});
+
+const port = process.env.PORT | 9090;
+server.listen(port, function(){
+    console.log('Listening at port '+port);
+});
