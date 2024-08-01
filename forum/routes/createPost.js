58ef713f2e0e0ead9327fb43757ff0463adb2e5a
Ernest Y. Balderosa <sofia_balderosa@dlsu.edu.ph>
2024-08-01 21:45:15 +0800

    update createpost.js

--- forum/routes/createPost.js
+++ forum/routes/createPost.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
 const sanitizeHtml = require('sanitize-html');
-const User = require('../models/User');
 
 // Middleware to check if user is authenticated
 const isAuthenticated = (req, res, next) => {
